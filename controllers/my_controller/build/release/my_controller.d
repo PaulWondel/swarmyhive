55dbef1065ae74fb873cde557f31fbde52804ad9
Rowalski <rowalski_wever@hotmail.com>
2020-05-20 16:07:17 +0200

    Basic movement/turning
    makes the bot turn if it detects anything close to it, keeps turning until it is far enough from the object

---
+++ controllers/my_controller/build/release/my_controller.d
@@ -0,0 +1,6 @@
+build/release/my_controller.o: my_controller.cpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files\ (x86)/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Motor.hpp
