c7987e9e180db7f23921b109e4b7bae73081ab41
Rowalski <rowalski_wever@hotmail.com>
2020-06-08 20:04:01 +0200

    movement
    rough movement with conditions on turning & straightening the bot. has some issues where it get stuck on rare occasions.

--- controllers/GPS4wheeler/GPS4wheeler.cpp
+++ controllers/GPS4wheeler/GPS4wheeler.cpp
@@ -5,17 +5,51 @@
 
 #define TIME_STEP 100
 using namespace webots;
-//using namespace std;
+using namespace std;
 
+bool isTurning = false;
+double leftSpeed1;
+double rightSpeed1;
+double leftSpeed2;
+double rightSpeed2;
+
+void turnRight(){
+      leftSpeed1 = 5;
+      rightSpeed1 = -10;
+      leftSpeed2 = 5;
+      rightSpeed2 = -10;
+}
+
+void turnLeft() {
+      leftSpeed1 = -10;
+      rightSpeed1 = 5;
+      leftSpeed2 = -10;
+      rightSpeed2 = 5;
+}
+
+void adjustRight() {
+  
+      leftSpeed1 = 1;
+      rightSpeed1 = -2;
+      leftSpeed2 = 1;
+      rightSpeed2 = -2;
+}
+
+void adjustLeft() {
+      leftSpeed1 = -2;
+      rightSpeed1 = 1;
+      leftSpeed2 = -2;
+      rightSpeed2 = 1;
+}
 int main(int argc, char **argv) {
   Robot *robot = new Robot();
   GPS *gp;
   gp = robot->getGPS("global");
   gp->enable(TIME_STEP);
   
-  DistanceSensor *ds[3];
-  char dsNames[3][11] = {"ds_front", "ds_right", "ds_left" };
-  for (int i = 0; i < 3; i++) {
+  DistanceSensor *ds[5];
+  char dsNames[5][11] = {"ds_front", "ds_right","ds_right_2", "ds_left", "ds_left_2" };
+  for (int i = 0; i < 5; i++) {
     ds[i] = robot->getDistanceSensor(dsNames[i]);
     ds[i]->enable(TIME_STEP);
   }
@@ -28,49 +62,35 @@ int main(int argc, char **argv) {
   }
   //int avoidObstacleCounter = 0;
   while (robot->step(TIME_STEP) != -1) {
-    double leftSpeed1 = 1.00;
-    double rightSpeed1 = 1.00;
-    double leftSpeed2 = 1.00;
-    double rightSpeed2 = 1.00;
     
-    std::cout<<"DS FRONT: "<<ds[0]->getValue()<<std::endl;
-    std::cout<<"DS R: "<<ds[1]->getValue()<<std::endl;
-    //std::cout<<"DS L: "<<ds[2]->getValue()<<std::endl;
-    std::cout<<"##############"<<std::endl;
-    
-    
-    if(ds[0]->getValue() < 900.0 && ds[1]->getValue() < 900.0) {
-      //std::cout<<"DS FRONT: "<<ds[0]->getValue()<<std::endl;
-      std::cout<<"SEES WALL AT FRONT AND RIGHT, TURNING LEFT"<<std::endl;
-      leftSpeed1 = -10;
-      rightSpeed1 = 10;
-      leftSpeed2 = -5;
-      rightSpeed2 = 5;
-    } 
-    else if(ds[0]->getValue() < 900.0 && ds[2]->getValue() < 900.0) {
-      //std::cout<<"DS FRONT: "<<ds[0]->getValue()<<std::endl;
-      std::cout<<"SEES WALL AT FRONT AND LEFT, TURNING RIGHT"<<std::endl;
-      leftSpeed1 = 10;
-      rightSpeed1 = -10;
-      leftSpeed2 = 5;
-      rightSpeed2 = -5;
-    } 
-    else if(ds[0]->getValue() < 900.0) {
-      //std::cout<<"DS FRONT: "<<ds[0]->getValue()<<std::endl;
-      std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<TURNING LEFT"<<std::endl;
-      leftSpeed1 = -10;
-      rightSpeed1 = 10;
-      leftSpeed2 = -5;
-      rightSpeed2 = 5;
+    if(!isTurning){ 
+      leftSpeed1 = 2.00;
+      rightSpeed1 = 2.00;
+      leftSpeed2 = 2.00;
+      rightSpeed2 = 2.00;
     }
-    else if(ds[1]->getValue() < 900.0){
-      //std::cout<<"DS R: "<<ds[2]->getValue()<<std::endl;
-      std::cout<<"<<<<<<<<<<<<<<<<<<<<<<<<<ADJUSTING LEFT"<<std::endl;
-      leftSpeed1 = -4;
-      rightSpeed1 = 4;
-      leftSpeed2 = 1;
-      rightSpeed2 = 1;
-    } 
+    
+    
+    if(ds[0]->getValue() < 800) {
+      if(ds[1]->getValue() < 1000 && ds[2]->getValue() <1000) {
+          isTurning = true;
+          turnLeft();
+      } else {
+          isTurning = true;
+          turnRight();
+      }
+   }else {
+      isTurning = false;
+    }
+    
+    if(ds[1]->getValue() <800 && ds[2]->getValue() > 800) {
+      adjustLeft();
+    }
+    
+    if(ds[3]->getValue() <800 && ds[4]->getValue() > 800) {
+      adjustRight();
+    }    
+    
     wheels[0]->setVelocity(leftSpeed1);
     wheels[1]->setVelocity(rightSpeed1);
     wheels[2]->setVelocity(leftSpeed2);
--- controllers/GPS4wheeler/build/release/GPS4wheeler.d
+++ controllers/GPS4wheeler/build/release/GPS4wheeler.d
@@ -1,7 +1,7 @@
 build/release/GPS4wheeler.o: GPS4wheeler.cpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/DistanceSensor.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Device.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/../../c/webots/types.h \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Motor.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Robot.hpp \
- /snap/webots/14/usr/share/webots/include/controller/cpp/webots/GPS.hpp
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files\ (x86)/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\ (x86)\Webots/include/controller/cpp/webots/GPS.hpp
