8a5fa48c4176c998277e0ca972fe19071078c4f0
Rowalski <rowalski_wever@hotmail.com>
2020-06-09 16:24:31 +0200

    turning logic
    turning logic needs fine tuning and last arguments

--- controllers/GPS4wheeler/GPS4wheeler.cpp
+++ controllers/GPS4wheeler/GPS4wheeler.cpp
@@ -12,6 +12,7 @@ double leftSpeed1;
 double rightSpeed1;
 double leftSpeed2;
 double rightSpeed2;
+int counter;
 
 void turnRight(){
       leftSpeed1 = 5;
@@ -30,16 +31,23 @@ void turnLeft() {
 void adjustRight() {
   
       leftSpeed1 = 1;
-      rightSpeed1 = -2;
+      rightSpeed1 = -1;
       leftSpeed2 = 1;
-      rightSpeed2 = -2;
+      rightSpeed2 = -1;
 }
 
 void adjustLeft() {
-      leftSpeed1 = -2;
+      leftSpeed1 = -1;
       rightSpeed1 = 1;
-      leftSpeed2 = -2;
+      leftSpeed2 = -1;
       rightSpeed2 = 1;
+}
+
+void goForward() {
+      leftSpeed1 = 6.00;
+      rightSpeed1 = 6.00;
+      leftSpeed2 = 6.00;
+      rightSpeed2 = 6.00;
 }
 int main(int argc, char **argv) {
   Robot *robot = new Robot();
@@ -49,7 +57,7 @@ int main(int argc, char **argv) {
   gp->enable(TIME_STEP);
   
   DistanceSensor *ds[5];
-  char dsNames[5][11] = {"ds_front", "ds_right1","ds_right2", "ds_left1", "ds_left2" };
+  char dsNames[5][11] = {"ds_front", "ds_right1","ds_right2", "ds_left2", "ds_left1" };
   for (int i = 0; i < 5; i++) {
     ds[i] = robot->getDistanceSensor(dsNames[i]);
     ds[i]->enable(TIME_STEP);
@@ -65,32 +73,69 @@ int main(int argc, char **argv) {
   while (robot->step(TIME_STEP) != -1) {
     
     if(!isTurning){ 
-      leftSpeed1 = 2.00;
-      rightSpeed1 = 2.00;
-      leftSpeed2 = 2.00;
-      rightSpeed2 = 2.00;
+      goForward();
+      counter++;
     }
     
+    // if(ds[0]->getValue() < 500) {
     
-    if(ds[0]->getValue() < 800) {
-      if(ds[1]->getValue() < 1000 && ds[2]->getValue() <1000) {
+    // printf("dolo");
+    // }
+    if(ds[0]->getValue() < 500) {
+      if(ds[1]->getValue() < 600 && ds[2]->getValue() <600) {
           isTurning = true;
+          cout<<"turning left"<<endl;
           turnLeft();
       } else {
           isTurning = true;
+          cout<<"turning right"<<endl;
           turnRight();
       }
    }else {
       isTurning = false;
     }
     
-    if(ds[1]->getValue() <800 && ds[2]->getValue() > 800) {
-      adjustLeft();
+    
+        if((ds[1]->getValue() > 300 && ds[1]->getValue() < 500) && (ds[2]->getValue() > 300 && ds[2]->getValue() < 500)) {
+      goForward();
+      
+      
+    }else if(counter > 2 && (ds[1]->getValue() <300 && ds[1]->getValue() >= ds[2]->getValue())){
+    /*
+    how to fix this, keep adjusting left until ds1 is bigger than ds2. need to fix if loops
+    
+    */
+    cout<<"adjusting left"<<endl;
+    adjustLeft();
+    counter = 0;
     }
     
-    if(ds[3]->getValue() <800 && ds[4]->getValue() > 800) {
+    if(counter > 2 && (ds[3]->getValue() <300 && ds[4]->getValue() < 300)) {
       adjustRight();
-    }    
+      cout<<"adjusting right"<<endl;
+      counter = 0;
+    }  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // if(counter > 2 && (ds[1]->getValue() <300 && ds[2]->getValue() < 300)) {
+      // adjustLeft();
+      // cout<<"adjusting left"<<endl;
+      // counter = 0;
+    // }
+    
+    // if(counter > 2 && (ds[3]->getValue() <300 && ds[4]->getValue() < 300)) {
+      // adjustRight();
+      // cout<<"adjusting right"<<endl;
+      // counter = 0;
+    // }    
     
     wheels[0]->setVelocity(leftSpeed1);
     wheels[1]->setVelocity(rightSpeed1);
@@ -98,7 +143,7 @@ int main(int argc, char **argv) {
     wheels[3]->setVelocity(rightSpeed2);
     
     //GPS PRINT COORDINATES
-    std::cout<<"X : "<<fabs(round((gp->getValues()[0]-0.0624)/0.125))<<"  ||  Z : "<<fabs(round((gp->getValues()[2]-0.0624)/0.125))<<std::endl; 
+    cout<<"X : "<<fabs(round((gp->getValues()[0]-0.0624)/0.125))<<"  ||  Z : "<<fabs(round((gp->getValues()[2]-0.0624)/0.125))<<std::endl; 
   }
   delete robot;
   return 0;  // EXIT_SUCCESS
