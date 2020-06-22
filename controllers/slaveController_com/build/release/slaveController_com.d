525c2885c00b06a224e3e19f6e1ce315fad98eb1
PaulWondel <0947421@hr.nl>
2020-06-22 23:46:49 +0200

    Update:
    - created new controllers serverBrain, slaveController_com

---
+++ controllers/slaveController_com/build/release/slaveController_com.d
@@ -0,0 +1,12 @@
+build/release/slaveController_com.o: slaveController_com.cpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Supervisor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Node.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Field.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Compass.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Receiver.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Emitter.hpp
