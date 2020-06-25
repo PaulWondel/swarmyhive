c4be84c22bc54fb259ccde65ad7fcf2e064e875c
Paul Wondel <gmprincep@gmail.com>
2020-06-25 16:30:49 +0200

    moved unused to _OLD_

---
+++ _OLD_/receiver/build/release/receiver.d
@@ -0,0 +1,9 @@
+build/release/receiver.o: receiver.cpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Supervisor.hpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Node.hpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Field.hpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Robot.hpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Receiver.hpp \
+ /snap/webots/14/usr/share/webots/include/controller/cpp/webots/Device.hpp \
+ extra.hpp
